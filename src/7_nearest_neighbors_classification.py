46b96d37d4608a5ebc8cab29cf70cf933daf5148
yollalala <yollandasekarrini@gmail.com>
2018-08-23 14:01:03 +0700

    change the problem

--- src/7_nearest_neighbors_classification.py
+++ src/7_nearest_neighbors_classification.py
@@ -1,41 +1,31 @@
-### nearest neighbors classification practice
+### k nearest neighbors classification practice
 ### tutorials from scikit-learn documentation
 ### this practice uses iris dataset on scikit-learn library
 
 # import the libraries
-from sklearn import neighbors, datasets, metrics
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn import datasets
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-
-n_neighbors = 15
 
 # load the iris datasets
 iris = datasets.load_iris()
 
-# get the first-two features
-X = iris.data[:-20, :2]
-y = iris.target[:-20]
+# get the first-two features datasets
+X_train, X_test, y_train, y_test = train_test_split(iris.data,
+	iris.target, test_size=0.15, random_state=42)
 
-# define the step size in mesh
-h = .02
+# build the model
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
 
-# create the color maps
-cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
-cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
-
-for weights in ['uniform', 'distance']:
-	# create the classifier
-	clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
-	clf.fit(X, y)
-	#y_pred = clf.predict(iris.data[-20:, :2])
-	print(clf.score(iris.data[-20:, :2], iris.target[-20:]))
-
-	
-	
-
-
-
-
-
+# print the classification report
+print('Classification report:\n%s' % classification_report(y_test, y_pred))
+# print the confusion matrix
+print('Confusion matrix:\n%s' % confusion_matrix(y_test, y_pred))
+# print the accuracy score
+print('Accuracy score:\n%s' % accuracy_score(y_test, y_pred))
 
